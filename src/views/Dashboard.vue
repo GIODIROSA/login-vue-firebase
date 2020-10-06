f1bbc01ea01ff68b38252667deff128758bb5144
Giovanni Di Rosa <giodirosaartes@gmail.com>
2020-10-05 21:33:55 -0300

    login finalizado

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="logOut">LoginOut</button>
+
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    logOut() {
+      firebase.auth().signOut()
+      .then(() => alert('Usuario deslogueados'))
+    }
+  },
+  created(){
+console.log(firebase.auth().currentUser)
+  }
+
+}
+</script>
+
+<style>
+
+</style>
